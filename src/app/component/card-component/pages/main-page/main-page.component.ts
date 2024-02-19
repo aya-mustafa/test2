de787950b62f3545bc7cee102ae70c52f7751868
aya-mustafa <ayamustafa408@gmail.com>
2024-02-19 06:37:14 +0200

    frist

---
+++ src/app/component/card-component/pages/main-page/main-page.component.ts
@@ -0,0 +1,34 @@
+import { Component,Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main-page',
+
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss']
+})
+export class MainPageComponent {
+  constructor(private _Router:Router)
+  {
+  }
+  @Input() cards :any = "";
+  isSelected:boolean= false;
+  currentElement:any;
+  arr: any[]=[];
+  selectCard(card: any,i:number ){
+    this.cards.forEach((c: { isSelected: boolean; }) => c.isSelected = false);
+    card.isSelected = true;
+    this.isSelected = true;
+    this.currentElement = this.cards[i]
+  }
+  okButton()
+  {
+    console.log(this.currentElement)
+    if(this.currentElement != undefined)
+    {
+      this.arr.push(this.currentElement);
+    }
+    console.log(this.arr);
+    this._Router.navigateByUrl('/dashboard')
+  }
+}
